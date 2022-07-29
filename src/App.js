3eb4a4ffc06387d398ad7877fa281c98a36d11af
Dave Kanter <dave.kanter@publicis.com>
2022-07-28 19:43:15 -0700

    Got it working with useCallback instead of the dreaded {...prop_spreading}

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   Container,
 } from 'semantic-ui-react';
@@ -6,9 +6,20 @@ import './App.css';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 import DisplayBalances from './components/DisplayBalances';
-import EntryLine from './components/EntryLine';
+import EntryLines from './components/EntryLines';
+
+let initialEntries; // initialize so it can be declared at bottom
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+
+  const deleteEntry = useCallback((id) => {
+    const result = entries.filter((entry) => entry.id !== id);
+    console.log('entries', entries);
+    console.log('result', result);
+    setEntries(result);
+  }, [entries]); // Using useCallback instead of passing the function as a prop.
+
   return (
     <Container>
 
@@ -16,8 +27,8 @@ function App() {
       <DisplayBalances />
 
       <MainHeader title="History" type="h3" />
-      <EntryLine description="Income" value="221.54" />
-      <EntryLine description="Expense" value="121.53" isExpense />
+
+      <EntryLines entries={entries} deleteEntry={deleteEntry} />
 
       <MainHeader title="Add New Transaction" type="h3" />
       <NewEntryForm />
@@ -27,3 +38,29 @@ function App() {
 }
 
 export default App;
+
+initialEntries = [{
+  id: 2,
+  description: 'Work income',
+  value: '1000.00',
+  isExpense: false,
+},
+{
+  id: 3,
+  description: 'Water Bill',
+  value: '121.00',
+  isExpense: true,
+},
+{
+  id: 4,
+  description: 'Rent',
+  value: '300.00',
+  isExpense: true,
+},
+{
+  id: 5,
+  description: 'ConEd',
+  value: '25.00',
+  isExpense: true,
+},
+];
--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Grid, Icon, Segment } from 'semantic-ui-react';
 
-function EntryLine({ description, value, isExpense = false }) {
+function EntryLine({
+  entry: {
+    id, description, value, isExpense = false,
+  }, deleteEntry,
+}) {
   return (
     <Segment color={isExpense ? 'red' : 'green'}>
       <Grid columns={3} textAlign="right">
@@ -12,7 +16,7 @@ function EntryLine({ description, value, isExpense = false }) {
         </Grid.Column>
         <Grid.Column width={3}>
           <Icon name="edit" bordered />
-          <Icon name="trash" bordered />
+          <Icon name="trash" bordered onClick={() => deleteEntry(id)} />
         </Grid.Column>
       </Grid>
     </Segment>
